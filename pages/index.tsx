97175c92c720913cb5f5c9b3e88c6b1f7002a7b3
StarfallV <envricochen10@gmail.com>
2022-08-04 11:12:59 +0700

    - Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/footer'
 import Navbar from '../components/navbar'
 
 const Home: NextPage = () => {
@@ -13,45 +14,47 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="flex w-full flex-1 flex-col items-start justify-center px-20 text-center text-gray-800 gap-4">
-          <div className="flex flex-row gap-4 items-center justify-center h-32">
-          <Image src="/logo_gov.png" alt="Vercel Logo" width={92} height={92} />
-          <h1 className="text-5xl font-bold">
-            Pemerintah Provinsi Maluku
-          </h1>
+        <main className="flex w-full flex-1 flex-row justify-center">
+          <div className="flex w-1/2 flex-1 flex-col items-start justify-center px-20 text-center text-gray-800 gap-7">
+            <div className="flex flex-row gap-4 items-center justify-center h-32">
+              <Image src="/logo_gov.png" alt="Vercel Logo" width={92} height={92} />
+              <h1 className="text-5xl font-bold">
+                Pemerintah Provinsi Maluku
+              </h1>
+            </div>
+
+            <div className="flex flex-col items-baseline">
+              <h1 className="text-6xl font-bold">
+                Klinik Konsultasi dan Layanan{' '}
+              </h1>
+              <h1 className="text-6xl font-bold text-blue-600">
+                ANJAB ABK
+              </h1>
+            </div>
+
+            <div className="flex items-baseline space-x-6">
+              <button className="bg-blue-700 hover:bg-blue-500 text-white font-semibold py-3 px-14 rounded-full transition ease-in-out dubration-300">
+                Masuk
+              </button>
+              <button className="bg-green-600 hover:bg-green-500 text-white font-semibold py-3 px-10 rounded-full transition ease-in-out duration-300">
+                Tentang kami
+              </button>
+            </div>
+
+            <div className="flex flex-col items-baseline">
+              <h1 className="text-2xl font-bold">
+                #BanggaMelayaniBangsa{' '}
+              </h1>
+            </div>
           </div>
 
-          <div className="flex flex-col items-baseline">
-          <h1 className="text-6xl font-bold">
-            Klinik Konsultasi dan Layanan{' '}
-          </h1>
-          <h1 className="text-6xl font-bold text-blue-600">
-            ANJAB ABK
-          </h1>
+          <div className="flex w-1/2 flex-1 flex-col items-start justify-center text-center text-gray-800 gap-7">
+            <div className="bg-[url('/bg/bg_illustration.png')] bg-cover w-full h-3/5">
+            </div>
           </div>
-
-          <div className="flex items-baseline space-x-6">
-            <button className="bg-blue-700 hover:bg-blue-500 text-white font-semibold py-3 px-14 rounded-full transition ease-in-out dubration-300">
-              Masuk
-            </button>
-            <button className="bg-green-600 hover:bg-green-500 text-white font-semibold py-3 px-10 rounded-full transition ease-in-out duration-300">
-              Tentang kami
-            </button>
-          </div>
-          
         </main>
 
-        <footer className="flex h-24 w-full items-center justify-center border-t">
-          <a
-            className="flex items-center justify-center gap-2"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </a>
-        </footer>
+         <Footer/>
       </div>
     </>
   )
